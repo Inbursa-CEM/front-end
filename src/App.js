0e0e71c68c93f8700478b93ad1619326eb963255
Diego Manja <94866755+Dmanjav@users.noreply.github.com>
2024-04-23 18:44:24 -0600

    created 1:1 button, added css, added color in App.css

--- src/App.js
+++ src/App.js
@@ -3,15 +3,12 @@ import Dashboard from "./App/Dashboard/dashboard";
 import Monitoreo from "./App/TablaMonitoreo/monitoreo";
 import Cursos from "./App/Cursos/cursos";
 import { Route, Routes } from "react-router-dom";
+import JoinCall from "./App/TablaMonitoreo/botonCall";
 
 function App() {
   return (
     <div className="App">
-      <Routes>
-        <Route path="/" element={<Monitoreo />} />
-        <Route path="/dashboard" element={<Dashboard />} />
-        <Route path="/cursos" element={<Cursos />} />
-      </Routes>
+      <JoinCall/ >
     </div>
   );
 }
