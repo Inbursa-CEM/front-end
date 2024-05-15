900db510ce13c51bd2cd96984c9f17b08c952048
Landa0911 <90887255+Landa0911@users.noreply.github.com>
2024-05-15 05:24:34 -0600

    Avance

--- src/App.js
+++ src/App.js
@@ -3,15 +3,17 @@ import Dashboard from "./App/Dashboard/dashboard";
 import Monitoreo from "./App/TablaMonitoreo/monitoreo";
 import Cursos from "./App/Cursos/cursos";
 import { Route, Routes } from "react-router-dom";
+import DetallesAgente from "./App/TablaMonitoreo/Detalles";
 
 function App() {
   return (
     <div className="App">
-      <Routes>
+     {/*  <Routes>
         <Route path="/" element={<Monitoreo />} />
         <Route path="/dashboard" element={<Dashboard />} />
         <Route path="/cursos" element={<Cursos />} />
-      </Routes>
+      </Routes> */}
+      <DetallesAgente />
     </div>
   );
 }
