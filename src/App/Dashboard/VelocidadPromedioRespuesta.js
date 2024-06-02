9605bcd237c3ad71c524b9d27b6154c8d5c5b513
Gustavo Gutiérrez <132943080+GAGV03@users.noreply.github.com>
2024-06-02 16:26:22 -0600

    Implementación de función para obtención de información velocidadRespuesta

--- src/App/Dashboard/VelocidadPromedioRespuesta.js
+++ src/App/Dashboard/VelocidadPromedioRespuesta.js
@@ -1,15 +1,45 @@
 import * as React from "react";
 import { Gauge, gaugeClasses } from "@mui/x-charts/Gauge";
-
-const settings = {
-  width: 200,
-  height: 200,
-  value: 2,
-  valueMax: 5,
-  valueMin: 0,
-};
+import { useState, useCallback, useEffect } from "react";
 
 export default function VelocidadPromedioRespuesta() {
+  const idSupervisor = sessionStorage.getItem("userId");
+  const [url, setUrl] = useState(
+    `http://localhost:8080/llamada/numLlamadasTotales?idSupervisor=${idSupervisor}`
+  );
+  const [velocidadPromedio, setVelocidadPromedio] = useState([0]);
+
+  const descargar = useCallback(() => {
+    console.log("Descargando datos");
+    fetch(url)
+      .then((response) => response.json())
+      .then((data) => {
+        setVelocidadPromedio(data.promedioRespuesta);
+      })
+      .catch((error) => console.log(error));
+  });
+
+  useEffect(() => {
+    // Llama a la función descargar inmediatamente al montar el componente
+    descargar();
+
+    // Configura el intervalo para llamar a descargar cada 10 minutos (600000 ms)
+    const interval = setInterval(() => {
+      descargar();
+    }, 600000);
+
+    // Limpia el intervalo al desmontar el componente
+    return () => clearInterval(interval);
+  }, []);
+
+  const settings = {
+    width: 200,
+    height: 200,
+    value: velocidadPromedio,
+    valueMax: 5,
+    valueMin: 0,
+  };
+
   return (
     <Gauge
       {...settings}
