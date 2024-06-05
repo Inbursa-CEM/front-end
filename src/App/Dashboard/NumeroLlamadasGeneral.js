e85596370fb3ae9a89305f4077b46e66b8e6db15
Gustavo Gutiérrez <132943080+GAGV03@users.noreply.github.com>
2024-06-04 23:54:03 -0600

    Cambios para obtener meta actual

--- src/App/Dashboard/NumeroLlamadasGeneral.js
+++ src/App/Dashboard/NumeroLlamadasGeneral.js
@@ -1,6 +1,6 @@
 //Autor: Gustavo Alejandro Gutiérrez Valdes Fecha: 11 de mayo de 2024
 
-import { Button,TextField } from "@mui/material";
+import { Button, TextField } from "@mui/material";
 import { Gauge, gaugeClasses } from "@mui/x-charts/Gauge";
 import * as React from "react";
 import { useState, useCallback, useEffect } from "react";
@@ -11,7 +11,11 @@ export default function NumeroLlamadasGeneral() {
   const [url, setUrl] = useState(
     `http://${process.env.REACT_APP_BACK_HOST}:8080/llamada/numLlamadasTotales?idSupervisor=${idSupervisor}`
   );
-  
+
+  const [metaUrl, setMetaUrl] = useState(
+    `http://${process.env.REACT_APP_BACK_HOST}:8080/usuario/meta?idSupervisor=${idSupervisor}`
+  );
+
   const [llamadas, setLlamadas] = useState([0]);
   const [meta, setMeta] = useState([100]);
   const [inputVisible, setInputVisible] = useState(false);
@@ -27,6 +31,16 @@ export default function NumeroLlamadasGeneral() {
       .catch((error) => console.log(error));
   });
 
+  const descargarMeta = useCallback(() => {
+    console.log("Descargando datos");
+    fetch(metaUrl)
+      .then((response) => response.json())
+      .then((data) => {
+        setMeta(data.meta);
+      })
+      .catch((error) => console.log(error));
+  });
+
   const cambiarMeta = useCallback(() => {
     console.log("Cambiando meta");
     fetch(
@@ -37,12 +51,13 @@ export default function NumeroLlamadasGeneral() {
         console.log(data);
       })
       .catch((error) => console.log(error));
-  })
+  });
 
-  //Primero se llama a la función descargar inmediatamente al montar el componente, Configura el intervalo para llamar a descargar cada 5 
+  //Primero se llama a la función descargar inmediatamente al montar el componente, Configura el intervalo para llamar a descargar cada 5
   //minutos (300000 ms) y se limpia el intervalo al desmontar el componente
   useEffect(() => {
     descargar();
+    descargarMeta();
     const interval = setInterval(() => {
       descargar();
     }, 300000);
@@ -58,11 +73,11 @@ export default function NumeroLlamadasGeneral() {
 
   //Esta función se encarga de cambiar la meta de llamadas y ocultar el campo de texto cuando se da Enter en el campo de texto
   const handleKeyDown = (event) => {
-    if(event.key === 'Enter'){
+    if (event.key === "Enter") {
       setInputVisible(false);
       cambiarMeta();
     }
-  }
+  };
   return (
     <div>
       {inputVisible ? (
